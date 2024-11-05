00af37b392020aa540218ecead386d7f40cfb69f
Luis Claudio Duarte Roxo <143570730+luisduarterx@users.noreply.github.com>
2024-11-05 00:41:14 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,42 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  //busca versao do banco de dados
+  const databaseVersionQuery = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionQuery.rows[0].server_version;
+
+  //busca maximo de conexoes do banco de dados
+  const databaseMaxConnectionsQuery = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsQuery.rows[0].max_connections;
+
+  //busca numero de conexoes abertas com banco de dados
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsQuery = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+  // `SELECT count(*)::int FROM pg_stat_activity WHERE datname='${databaseName}';`,
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsQuery.rows[0].count;
+  console.log();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
