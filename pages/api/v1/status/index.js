b1fcb6c182b430ba350f2957a759b37b0566c5c7
Luis Claudio Duarte Roxo <143570730+luisduarterx@users.noreply.github.com>
2025-11-13 20:40:14 +0000

    alterando /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,18 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const updateAt = new Date().toISOString()
+  const databaseName = process.env.POSTGRES_DB;
+  const updateAt = new Date().toISOString();
   const databaseVersion = await database.query("SHOW server_version");
-  const databaseMaxConnections = await database.query("SHOW max_connections")
-  const databaseOpennedConnections = await database.query("SELECT count(*)::int from pg_stat_activity WHERE datname='local_db'")
-  
-  response.status(200).json({ update_at: updateAt,
-    dependencies:
-      {
-        database:{
-          version:databaseVersion.rows[0].server_version,
-          max_connections: Number(databaseMaxConnections.rows[0].max_connections),
-          openned_connections: Number(databaseOpennedConnections.rows[0].count)
-  }}});
+  const databaseMaxConnections = await database.query("SHOW max_connections");
+  const databaseOpennedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: Number(databaseMaxConnections.rows[0].max_connections),
+        openned_connections: Number(databaseOpennedConnections.rows[0].count),
+      },
+    },
+  });
 }
 export default status;
